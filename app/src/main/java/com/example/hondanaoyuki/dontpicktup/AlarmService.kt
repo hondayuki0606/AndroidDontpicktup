d17e93dad1aba582db49a9cbf38a6f1d555e5439
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-05-04 22:30:03 +0900

    Commit

--- app/src/main/java/com/example/hondanaoyuki/dontpicktup/AlarmService.kt
+++ app/src/main/java/com/example/hondanaoyuki/dontpicktup/AlarmService.kt
@@ -1,16 +1,20 @@
 package com.example.hondanaoyuki.dontpicktup
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.IBinder
 
 class AlarmService : Service(), SensorEventListener {
 
-    override fun onBind(intent: Intent?): IBinder? {
-        return null
+    override fun onDestroy() {
+        super.onDestroy()
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.unregisterListener(this)
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
@@ -21,4 +25,7 @@ class AlarmService : Service(), SensorEventListener {
 
     }
 
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 }
