73f6cfc755ce92cf597c4bcb90059f8b09fa01b5
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-05-04 22:18:10 +0900

    Commit Initialize

--- app/src/main/java/com/example/hondanaoyuki/dontpicktup/AlarmService.kt
+++ app/src/main/java/com/example/hondanaoyuki/dontpicktup/AlarmService.kt
@@ -1,4 +1,24 @@
 package com.example.hondanaoyuki.dontpicktup
 
-class AlarmService {
+import android.app.Service
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.os.IBinder
+
+class AlarmService : Service(), SensorEventListener {
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
 }
